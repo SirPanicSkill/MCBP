a6ccd496cab6be0d76b68cef15c121352a0e79eb
SirPanicSkill <txwnik@live.ru>
2018-07-26 18:11:54 +0300

    edited vue out

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,24 @@
     <header class="header">
       <nav class="menu">
         <ul class="menu__wrapper">
-          <li class="menu__list"><router-link to="/">Главная</router-link></li>
-          <li class="menu__list"><router-link to="about">О Компании</router-link></li>
+          <li class="menu__list"><router-link class="menu__link" to="/">Главная</router-link></li>
+          <li class="menu__list"><router-link class="menu__link" to="about">О Компании</router-link></li>
         </ul>
       </nav>
     </header>
-    <transition name="fade">
-      <router-view/>
-    </transition>
+    <main class="main">
+      <transition name="fade">
+        <router-view/>
+      </transition>
+    </main>
     <footer class="footer">I am footer</footer>
   </div>
 </template>
 
 <script>
+import './css/normalize.css';
+import './css/main.css';
+
 export default {
   name: 'App'
 }
